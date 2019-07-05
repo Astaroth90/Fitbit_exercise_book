c7bff91ac4e0bf429b60da7d5fc9c0702c6e69df
Philipp Sossong <Psossong@gmx.de>
2019-07-05 12:42:59 +0200

    Create switch between two screens. Adding Icon with click event TODO: Build start and selectingscreen

--- app/index.js
+++ app/index.js
@@ -9,11 +9,18 @@ import { vibration } from "haptics";
 
 import exercises from "./exercises.js";
 
-//const svgStart = document.getElementById("svgStart");
+const svgStart = document.getElementById("svgStart");
+const svgSelect = document.getElementById("svgSelect");
+let myImg = document.getElementById("img");
 
+myImg.onclick = function (e) {
+    svgStart.style.display = "none";
+    svgSelect.style.display = "inline";
+}
 var exer = new exercises("Bauch",10,20,5);
 
 let demotext = document.getElementById("demotext");
 demotext.text = exer.Name;
+
 
 console.log('Hello world!');
