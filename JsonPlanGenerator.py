816bcf8ddfaca7d024e941b648e145646edac13b
Astaroth90 <Psossong@gmx.de>
2019-09-25 18:23:26 +0200

    Update to Python3
    Update to Python3
    GUI for PlanGeneration

--- JsonPlanGenerator.py
+++ JsonPlanGenerator.py
@@ -1,7 +1,13 @@
 import json
 #import Tkinter
-from Tkinter import *
+from tkinter import *
 
+def button_quit_action(): fenster.quit()
+#def addButton_action()
+	
+def button_create_action():
+	print("create")
+	
 print("Start")
 testplan = {}
 testplan['name'] = []
@@ -37,7 +43,40 @@ file.write(plan)
 file.close()
 
 fenster = Tk()
-fenster.title("Nur ein Fenster")
+fenster.title("Create your own Plan")
+
+create_button = Button(fenster, text="Create", command= button_create_action)
+
+exit_button = Button(fenster, text="Close", command=button_quit_action)
+
+start_label = Label(fenster, text="Plan")
+title_label = Label(fenster,text="Name")
+repeat_label = Label(fenster,text="Repeat")
+sets_label = Label(fenster,text="Sets")
+weight_label = Label(fenster,text="Weight")
+list_label = Label(fenster,text="hier steht \n\
+eine Liste aller \n\
+Ubungen")
+
+name_entry = Entry(fenster, bd=5, width=40)
+repeat_entry = Entry(fenster, bd=5, width= 40)
+sets_entry = Entry(fenster, bd=5, width=40)
+weight_entry = Entry(fenster,bd=5, width=40)
+
+start_label.grid(row=0, column=1)
+title_label.grid(row=1, column=1)
+name_entry.grid(row=1, column=2,columnspan=2)
+list_label.grid(row=1,column=0, rowspan=4)
+sets_label.grid(row=2,column=1)
+sets_entry.grid(row=2,column=2,columnspan=2)
+repeat_label.grid(row=3,column=1)
+repeat_entry.grid(row=3,column=2,columnspan=2)
+weight_label.grid(row=4,column=1)
+weight_entry.grid(row=4,column=2,columnspan=2)
+
+exit_button.grid(row=5, column=1)
+
+
 fenster.mainloop()
 
 
